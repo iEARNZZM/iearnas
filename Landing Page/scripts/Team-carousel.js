bea62f2426306d29ca5241e9ed9d337c480ebd81
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2020-06-03 17:00:05 +0300

    Tvarkausi

---
+++ Landing Page/scripts/Team-carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    alert('loaded');
+    $('.team-carousel').slick();
+})
+//https://github.com/kenwheeler/slick/tree/master/slick
+//https://www.youtube.com/watch?v=hyr2zasS27w
+//https://www.youtube.com/watch?v=YI1SZVxyMc4
+//
