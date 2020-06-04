8ca56a8c6ade49eb24bc9caed0756edbee41fd8d
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2020-06-05 00:06:05 +0300

    Dirbam
    Padėjau Projektų tinklapio dalies pavizdy ant kurio ir į kurį galima dirbti

--- Landing Page/scripts/Team-carousel.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    alert('loaded');
-    $('.team-carousel').slick();
-})
-//https://github.com/kenwheeler/slick/tree/master/slick
-//https://www.youtube.com/watch?v=hyr2zasS27w
-//https://www.youtube.com/watch?v=YI1SZVxyMc4
-//
---
+++ Landing Page/scripts/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#bobas .container-3 .carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 7000,
+        speed: 1000,
+        dots: true,
+        arrows: true,
+        
+        
+    });
+});
