abfb847e6ceffcd6d8aac4573704f370146d7b55
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2020-06-22 18:55:33 +0300

    Pakeičiau Kurti projekto info menu
    Sudėjau info menu stilių ir feture, kad būtų galima pakeist info dežutės plotį su pačia pelyte tempiant pačią težutę

---
+++ scripts/split.js
@@ -0,0 +1,6 @@
+Split (['.writing', '.info'],{
+    gutterSize:15,
+    sizes: [80, 20],      // Pradinis pirmos ir antros dežutes plotis (%)
+    minSize: [0, 300],  // Mažiausias pirmos ir antros dežutes plotis (px)
+    snapOffset: 5,
+});
